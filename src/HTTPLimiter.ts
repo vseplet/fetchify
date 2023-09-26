5ebccb73b4affbcc7a1e967ec1a19b213cdc54c9
Sevapp <mr.sevapp@gmail.com>
2023-09-26 14:57:45 +0600

    create new loop based on rps

--- src/HTTPLimiter.ts
+++ src/HTTPLimiter.ts
@@ -1,5 +1,7 @@
 import { delay } from "../deps.ts";
 
+type FetchInput = URL | Request | string;
+
 interface IRequestEntity {
   promise: Promise<Response>;
   init?: RequestInit;
@@ -9,40 +11,58 @@ interface IRequestEntity {
 }
 
 interface IHTTPLimiterOptions {
+  rps: number; // requests per second
   interval: number;
 }
 
-type FetchInput = URL | Request | string;
-
 export class HTTPLimiter {
   #options: IHTTPLimiterOptions = {
+    rps: 1,
     interval: 0,
   };
 
   #queue: IRequestEntity[] = [];
+  #iterationStartTime = 0;
+  #requestsPerIteration = 0;
 
   constructor(options?: Partial<IHTTPLimiterOptions>) {
-    if (options) {
-      for (const [key, value] of Object.entries(options)) {
-        // @ts-ignore
-        if (key in this.#options) this.#options[key] = value;
-      }
+    if (options)
+    {
+      this.#options = { ...this.#options, ...options };
     }
   }
 
   async #loop() {
-    while (true) {
+    this.#iterationStartTime = new Date().getTime();
+
+    while (true)
+    {
       const entity = this.#queue.shift();
       if (!entity) break;
-
-      try {
-        const response = await fetch(entity.input, entity.init);
-        entity.resolve(response);
-      } catch (error) {
-        entity.reject(error);
+      if (this.#requestsPerIteration >= this.#options.rps)
+      {
+        await delay(this.#options.interval);
+        continue;
       }
 
-      await delay(this.#options.interval);
+      this.#requestsPerIteration ++;
+      fetch(entity.input, entity.init)
+        .then(resp => {
+          entity.resolve(response);
+        })
+        .catch(err => {
+          entity.reject(error);
+        })
+        .finally(() => {
+          this.#requestsPerIteration --;
+        });
+
+      if (this.#requestsPerIteration == this.#options.rps)
+      {
+        const timeOffset = this.#iterationStartTime + 1000 - new Date().getTime();
+        await delay(timeOffset);
+        this.#iterationStartTime = new Date().getTime();
+      }
     }
   }
 
